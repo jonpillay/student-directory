5fd7ef64bcc92da56b2205cd893d28ff940c8b3e
Jon P <jon.pillay@gmail.com>
2023-03-07 11:48:37 +0000

    refactored student count

--- directory.rb
+++ directory.rb
@@ -1,3 +1,5 @@
+student_count = 11
+
 #print out the names of all students
 puts "The students of Villains Academy"
 puts "-------------"
@@ -13,8 +15,12 @@ puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
 
+puts "In total, we have #{student_count} exceptional students"
+
+=begin
 #print out the total number of students
 print "In total, we have "
 #use the print() function to put/print to avoid seperating lines
 print 11
 puts " great students"
+=end
