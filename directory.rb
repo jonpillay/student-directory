246798754956439d7c5f30c9cb49ccf698968ea3
Jon P <jon.pillay@gmail.com>
2023-03-07 16:37:15 +0000

    Added input_student method

--- directory.rb
+++ directory.rb
@@ -27,6 +27,26 @@ def print_footer(students)
   puts "In total, we have #{students.count} exceptional students"
 end
 
+def input_students
+  puts "Please enter the name of the student you would like to enroll. "
+  puts "To finish entering names, hit return twice"
+
+  students = []
+
+  name = gets.chomp
+
+  while !name.empty? do
+    students << {name: name, cohort: :november}
+    puts "The total number of student is now #{students.count}"
+    name = gets.chomp
+  end
+
+  return students
+
+end
+
+
+students = input_students
 print_header
 print_names(students)
 print_footer(students)
