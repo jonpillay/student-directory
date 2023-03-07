4634f92e95cfb362ec6cb485f7c370af0aa2954d
Jon P <jon.pillay@gmail.com>
2023-03-07 11:43:57 +0000

    added directory.rb and list of students

---
+++ directory.rb
@@ -0,0 +1,20 @@
+#print out the names of all students
+puts "The students of Villains Academy"
+puts "-------------"
+puts "Dr. Hannibal Lecter"
+puts "Darth Vader"
+puts "Nurse Ratched"
+puts "Michael Corleone"
+puts "Alex DeLarge"
+puts "The Wicked Witch of the West"
+puts "Terminator"
+puts "Feddy Kruger"
+puts "The Joker"
+puts "Joffrey Baratheon"
+puts "Norman Bates"
+
+#print out the total number of students
+print "In total, we have "
+#use the print() function to put/print to avoid seperating lines
+print 11
+puts " great students"
